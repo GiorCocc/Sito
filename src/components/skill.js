3a9db45120dcb6e46f83f495dd828bd10da12ea7
Giorgio Coccapani <giorcocc@gmail.com>
2021-02-03 20:51:27 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'gc_sito/master'.
    
    Changes to be committed:
    	modified:   src/components/skill.js

--- src/components/skill.js
+++ src/components/skill.js
@@ -4,12 +4,12 @@ export default function Skills(){
     return(
         <div className="p-5 grid gap-4 sm:grid-cols-1 md:grid-cols-2">
             <div className="pb-5">
-                <div className="flex flex-row h-full">
-                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#E34F26" className="w-9 h-full">
+                <div className="flex flex-row">
+                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#E34F26" className="w-9 h-9">
                         <title>HTML5 icon</title>
                         <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/>
                     </svg>
-                    <p className="text-xl justify-center align-middle ml-5 text-gray-200">HTML</p>
+                    <p className="text-xl h-9 justify-center align-middle ml-5 text-gray-200">HTML</p>
                 </div>
                 <div className="bg-gray-400 rounded-md">
                     <div className="bg-html h-10 w-9/12 rounded-md justify-items-center">
