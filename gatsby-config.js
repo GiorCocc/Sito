dc0c052eec36f1942914948997d20005b6b4d5ae
Giorgio Coccapani <giorcocc@gmail.com>
2021-02-05 19:36:17 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'gc_sito/master'.
    
    Changes to be committed:
    	modified:   gatsby-config.js
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/img/icon.png

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,32 @@
 
 module.exports = {
   siteMetadata:{
-    title:`Titolo`
+    title: `Giorgio Coccapani`,
+    description: `Sito personale di Giorgio Coccapani`,
+    author: `Giorgio Coccapani`,
   },
   /* Your site config here */
   plugins: [
     {
       resolve: "gatsby-plugin-react-svg",
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Giorgio Coccapani`,
+        short_name: `GC`,
+        description: `Sito personale di Giorgio Coccapani`,
+        lang: `it`,
+        display: `standalone`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/img/icon.png`, // This path is relative to the root of the site.
+      }, 
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     'gatsby-plugin-postcss'
   ],
 }
